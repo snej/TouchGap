310a1d71f3dde698e0b640738b6f59727aadf41e
Chris Anderson <jchris@apache.org>
2012-12-28 14:48:05 -0800

    an experiment in performance - fastclick works

--- www/js/app.js
+++ www/js/app.js
@@ -1,30 +1,64 @@
 $(function() {
   var config = require('./app/config'),
+    home = require("./app/home"),
+    chat = require("./app/chat"),
+    fastclick = require("fastclick"),
+    router = require('director').Router;
 
+  new fastclick.FastClick(document.body);
 
-
-  var route = require('./route'),
-
-    auth = require('./app/auth'),
-    // sync = require('./app/sync'),
-    // register controllers
-    home = require("./app/home")(route),
-    wikis = require("./app/wikis")(route),
-    edits = require("./app/edits")(route),
-    sidebar = require("./app/sidebar")(route);
-
-  home.ready();
-  sidebar.draw();
-
-  coux.changes(config.dbUrl, function(err, changes) {
-      console.log("change", err, changes);
-      sidebar.draw();
-
-      var matches = window.location.toString().match(/^[^#]*#(.+)$/);
-      if (matches && matches[1] && !/edit/.test(matches[1])) {
-          route.go(matches[1])
+  var content = $("#content")[0],
+    contentRoutes = {
+      "/login" : home.login,
+      "/reload" : location.reload,
+      "/start" : home.start,
+      "/chat/:id" : chat.view,
+      "/" : function() {
+        $(content).append('<li>/</li>')
+      },
+      "/bar" : function() {
+        $(content).append('<li>/bar :/</li>')
+      },
+      "/ok" : function() {
+        $(content).append('<li>/ok :)</li>')
       }
-  });
+    },
+    sidebar = $("#sidebar")[0],
+    sidebarRoutes = {
+      "/chats" : chat.index
+    };
+
+  // $(document.body).click(function() {
+  //   $(content).append('<li>** clicked</li>')
+  //   // return false;
+  // })
+
+  var contentRouter = router(contentRoutes, content);
+  // // var sidebarRouter = router(sidebarRoutes, sidebar);
+  // // var route = require('./route'),
+
+  contentRouter.init("/start");
+
+  //   auth = require('./app/auth'),
+  //   // sync = require('./app/sync'),
+  //   // register controllers
+  //   home = require("./app/home")(route),
+  //   wikis = require("./app/wikis")(route),
+  //   edits = require("./app/edits")(route),
+  //   sidebar = require("./app/sidebar")(route);
+
+  // home.ready();
+  // sidebar.draw();
+
+  // coux.changes(config.dbUrl, function(err, changes) {
+  //     console.log("change", err, changes);
+  //     sidebar.draw();
+
+  //     var matches = window.location.toString().match(/^[^#]*#(.+)$/);
+  //     if (matches && matches[1] && !/edit/.test(matches[1])) {
+  //         route.go(matches[1])
+  //     }
+  // });
 
 });
 
